aee12093c4f17d0f2daf2e6a01e1df0b65c0b4ae
Mayhem-95 <135868086+Mayhem-95@users.noreply.github.com>
2023-07-08 22:56:36 +0300

    Random number estimation

---
+++ Random number estimation.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  5 23:56:08 2023
+
+@author: omarm
+"""
+
+user_estimation = input("Please estimate a value between (1, 100): ")
+user_estimation = int(user_estimation)
+import random
+random_value=(random.randint(1, 100))
+random_value=int(random_value)
+print(random_value)
+while user_estimation != random_value:
+    print(user_estimation)
+    user_estimation = (int(input("Guess again: ")))
+    random_value=(random.randint(1, 10))
+    random_value=int(random_value)
+    print(random_value)
+    if user_estimation > random_value:
+        print("you're too high.Guess again")
+    elif user_estimation < random_value:
+        print("you're too low.Guess again")
+    elif user_estimation == random_value:
+        print("Congratulation! You guessed the number correctly!")
+
